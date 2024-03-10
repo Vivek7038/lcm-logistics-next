448d918f61ff50d44a1ead0af49fd82754bc3f13
Vivek7038 <vivek888chavan@gmail.com>
2024-03-10 08:38:42 +0530

    added bg color to nav when scroll above 90

--- app/_components/Navbar.jsx
+++ app/_components/Navbar.jsx
@@ -1,5 +1,6 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React,{useState} from "react";
 import MenuIcon from "@mui/icons-material/Menu";
 import Link from "next/link";
 const data = [
@@ -21,17 +22,26 @@ const data = [
   },
 ];
 const Navbar = () => {
+  const [color, setColor] = useState(true)
+  const changeColor = () => {
+      if (window.scrollY >= 90) {
+          setColor(true)
+      } else {
+          setColor(false)
 
+      }
+  }
+  window.addEventListener("scroll",changeColor);
   return (
-    <main className="min-h-30 max-w-[2500px]  fixed  ml-0 w-[100%] flex justify-center text-white ">
+    <main className={`z-[9999] min-h-30 max-w-[2500px]  fixed  ml-0 w-[100%] flex justify-center text-white ${color ? "bg-black":""}`}>
       <div className="flex flex-row  items-center pt-10 gap-5 w-[100%] 2xl:w-[80%]   justify-between">
         <div className="font-bold text-[18px] flex items-center  ">
           <img src={"./logo.png"} alt={"logo"} className={"h-8 w-8 mr-4"}></img>
-            <div className={"text-2xl text-black"}>
+            <div className={`text-2xl text-black ${color ? "text-white": ""}`}>
           LCM.PVT.LTD
             </div>
         </div>
-        <div className="flex flex-row  gap-x-12 cursor-pointer">
+        <div className="hidden md:flex flex-row  gap-x-12 cursor-pointer">
           {data.map((item) => (
               <h1><Link href={item.link} >{item.label}</Link></h1>
           ))}
