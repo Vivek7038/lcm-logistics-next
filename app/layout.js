88dc80eb17609ad3c0f4830468cc287ad49fece1
Rahul <rahulkrish321123@gmail.com>
2024-03-11 20:25:04 +0530

    added font family error rectified

--- app/layout.js
+++ app/layout.js
@@ -14,11 +14,17 @@ const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={"dar"} >
-        <Navbar />
-        {children}
-      <Footer/>
-      </body>
+    <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+              rel="stylesheet"/>
+    </head>
+    <body className={"dar"}>
+    <Navbar/>
+    {children}
+    <Footer/>
+    </body>
     </html>
   );
 }
