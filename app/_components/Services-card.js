32b1ef8d04bb31dd744fc7ba9046b08ce3bb4697
Vivek7038 <vivek888chavan@gmail.com>
2024-03-11 08:58:00 +0530

    added navigation to about us and services page, about us seperate component

--- app/_components/Services-card.js
+++ app/_components/Services-card.js
@@ -1,4 +1,10 @@
+"use client";
+import { useRouter } from "next/navigation"
 export default function Card(props) {
+    const router=useRouter()
+    const handleClick=()=>{
+        router.push("/services")
+    }
     return (
         <div className={"w-[355px] h-[410px]  relative shadow-lg "}>
             <div >
@@ -11,7 +17,7 @@ export default function Card(props) {
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisiciquaerat quisquam cum ratione ea aliquid qui hic recusandae atque corporisng elit. Inventore,
                 </p>
-                <a className={"absolute font-medium right-4 bottom-4 cursor-pointer"}>
+                <a className={"absolute font-medium right-4 bottom-4 cursor-pointer"} onClick={()=>handleClick()}>
                     READ MORE
                 </a>
             </div>
