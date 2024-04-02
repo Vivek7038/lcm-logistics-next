7ea30fda07e4a01ff6ca64a0a38f0edfd91a4cba
Vivek7038 <vivek888chavan@gmail.com>
2024-04-02 11:56:29 +0530

    update bg of nav

--- app/_components/Navbar.jsx
+++ app/_components/Navbar.jsx
@@ -63,16 +63,16 @@ const Navbar = () => {
   }, []);
   return (
     <main
-      className={`z-[9999] min-h-26 max-h-18  min-w-[100vw] fixed  lg:w-[100vw]  text-white ${
-        color ? "bg-black opacity-50" : ""
-      }`}
+      className={`z-[9999] min-h-26 max-h-18 min-w-[100vw] fixed lg:w-[100vw] text-white ${
+        color ? "bg-black bg-opacity-50" : ""
+      } backdrop-blur-[5px] rounded-[12px] border-0 border-solid border-[rgba(255,255,255,0.75)]`}
     >
       <div className="flex flex-row  items-center  justify-between py-3 px-4  lg:px-20">
         <div className="font-bold text-[18px] flex items-center justify-between">
           <img
             src={"./logo-1.png"}
             alt={"logo"}
-            className={"h-14 w-24 pr-2 cursor-pointer"}
+            className={"h-14 w-32 pr-2 cursor-pointer"}
             onClick={() => router.push("/")}
           ></img>
           {/* <div
